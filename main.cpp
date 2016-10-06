5d10569b1f4c267cc4274aa34c91a1cd01aa1686
1455422 <Benson Chu>
2016-10-06 12:00:17 -0500

    Allow lazy file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
-int main()
+#include <iostream>
+using namespace std;
+
+int main(int argc, char ** argv)
 {
+    string file = "";
+
+    if(argc > 1)
+    {
+        string arg = argv[1];
+        file = arg.substr(6);
+    }
+    else
+    {
+        file = "input.txt";
+    }
 
 }
