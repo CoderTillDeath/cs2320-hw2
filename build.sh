a98053c650e4ef32e8feac0b1ccf0078930c51f7
1455422 <Benson Chu>
2016-10-06 12:36:10 -0500

    Modularize build process

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-echo "Environment: `uname -a`"
-echo "Compiler: `$CXX --version`"
-
-$CXX -std=c++11 *cpp -o coogsive || exit 1
-
 for A in a b c d e f g h i j k l m n o p q r s t ; 
     do echo "Test ${A}";
         ./coogsive "input=test-cases/test-${A}.1.input1" | diff - test-cases/test-${A}.out.txt ;
