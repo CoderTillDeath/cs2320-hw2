acffe530361b43a4f5a8bb2d16a663183b5a9d0e
1455422 <Benson Chu>
2016-10-06 12:33:44 -0500

    Editted build script to fit problem

--- build.sh
+++ build.sh
@@ -3,13 +3,10 @@
 echo "Environment: `uname -a`"
 echo "Compiler: `$CXX --version`"
 
-$CXX -std=c++11 *cpp -o notify || exit 1
-echo ""
-echo "test a"
-./notify "test-a.1.txt" | diff - test-a.out.txt || exit 1
-echo ""
-echo "test b"
-./notify "test-b.1.txt" | diff - test-b.out.txt || exit 1
-echo ""
-echo "test z"
-./notify "test-z.1.txt" | diff - test-z.out.txt || exit 1
+$CXX -std=c++11 *cpp -o coogsive || exit 1
+
+for A in a b c d e f g h i j k l m n o p q r s t ; 
+    do echo "Test ${A}";
+        ./coogsive "input=test-cases/test-${A}.1.input1" | diff - test-cases/test-${A}.out.txt ;
+        echo "" ;
+        done
